837caeb6bdf0309476310396b682d210d85e6bcd
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-07 14:39:33 -0700

    Copying over

--- source.js
+++ source.js
@@ -1,3 +1,5 @@
-export default function tempLate (alpha: mixed): mixed {
-  return alpha
+import reduceWithValueKey from "@unction/reducewithvaluekey"
+
+export default function reduceValues (unction: AccumulatedType => ValueType => AccumulatedType): Function {
+  return reduceWithValueKey((accumulated: AccumulatedType): Function => (value: ValueType): Function => (): AccumulatedType => unction(accumulated)(value))
 }
--- test.js
+++ test.js
@@ -1,12 +1,25 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import tempLate from "./"
+import reduceValues from "./"
 
-test(({same, end}) => {
-  same(
-    tempLate(true),
-    false
+test(({equal, end}) => {
+  equal(
+    reduceValues((accumulation) => (current) => `${accumulation}/${current}`)(".")(["a", "b", "c"]),
+    "./a/b/c"
+  )
+
+  end()
+})
+
+test(({equal, end}) => {
+  equal(
+    reduceValues((accumulation) => (current) => `${accumulation}/${current}`)(".")({
+      aaa: "a",
+      bbb: "b",
+      ccc: "c",
+    }),
+    "./a/b/c"
   )
 
   end()
