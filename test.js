2b217bb96f378f5af7b5e9b1929f83b78119b6c2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import reduceValues from "./source.js"
+import recordFrom from "./source"
 
 test(({equal, end}) => {
   equal(
