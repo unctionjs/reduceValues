79faffa289e26a73a181eda3a4fe71c23bbb7e7a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-13 19:50:54 -0700

    :lint:

--- source.js
+++ source.js
@@ -1,5 +1,9 @@
 import reduceWithValueKey from "@unction/reducewithvaluekey"
 
 export default function reduceValues (unction: AccumulatedType => ValueType => AccumulatedType): Function {
-  return reduceWithValueKey((accumulated: AccumulatedType): Function => (value: ValueType): Function => (): AccumulatedType => unction(accumulated)(value))
+  return reduceWithValueKey(
+    (accumulated: AccumulatedType): Function =>
+      (value: ValueType): Function =>
+        (): AccumulatedType => unction(accumulated)(value)
+  )
 }
