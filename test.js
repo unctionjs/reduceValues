91b815f76dd236a56b98722f1954027064455a93
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-23 04:19:24 -0700

    Now we can handle maps and sets

--- test.js
+++ test.js
@@ -5,7 +5,15 @@ import reduceValues from "./source"
 
 test(({equal, end}) => {
   equal(
-    reduceValues((accumulation) => (current) => `${accumulation}/${current}`)(".")(["a", "b", "c"]),
+    reduceValues(
+      (accumulation) =>
+        (current) =>
+          `${accumulation}/${current}`
+    )(
+      "."
+    )(
+      ["a", "b", "c"]
+    ),
     "./a/b/c"
   )
 
@@ -14,11 +22,53 @@ test(({equal, end}) => {
 
 test(({equal, end}) => {
   equal(
-    reduceValues((accumulation) => (current) => `${accumulation}/${current}`)(".")({
-      aaa: "a",
-      bbb: "b",
-      ccc: "c",
-    }),
+    reduceValues(
+      (accumulation) =>
+        (current) =>
+          `${accumulation}/${current}`
+    )(
+      "."
+    )(
+      {
+        aaa: "a",
+        bbb: "b",
+        ccc: "c",
+      }
+    ),
+    "./a/b/c"
+  )
+
+  end()
+})
+
+test(({equal, end}) => {
+  equal(
+    reduceValues(
+      (accumulation) =>
+        (current) =>
+          `${accumulation}/${current}`
+    )(
+      "."
+    )(
+      new Map([["aaa", "a"], ["bbb", "b"], ["ccc", "c"]])
+    ),
+    "./a/b/c"
+  )
+
+  end()
+})
+
+test(({equal, end}) => {
+  equal(
+    reduceValues(
+      (accumulation) =>
+        (current) =>
+          `${accumulation}/${current}`
+    )(
+      "."
+    )(
+      new Set(["a", "b", "c"])
+    ),
     "./a/b/c"
   )
 
